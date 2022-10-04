27aa6fee3e7cf5cde36e692519968f61e09c1c81
Valentin Youshkevich <valiantsin.yushkevich@playgendary.com>
2022-10-04 10:29:08 +0300

    repeir list style

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,33 +1,11 @@
 <template>
   <v-col cols="12" md="4">
     <v-card max-width="450" class="mx-auto">
-      <!-- <v-list three-line>
-        <template v-for="(item, index) in users">
-          <v-subheader v-if="item.header" :key="item.header" v-text="item.header"></v-subheader>
-
-          <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
-
-          <v-list-item v-else :key="item.title">
-            <v-list-item-avatar>
-              <v-img :src="item.avatar"></v-img>
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title v-html="item.title"></v-list-item-title>
-              <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+      <v-list :items="users" item-props lines="three">
+        <template v-slot:subtitle="{ subtitle }">
+          <div v-html="subtitle"></div>
         </template>
-      </v-list> -->
-      <v-list
-      :items="users"
-      item-props
-      lines="three"
-    >
-      <template v-slot:subtitle="{ subtitle }">
-        <div v-html="subtitle"></div>
-      </template>
-    </v-list>
+      </v-list>
     </v-card>
 
   </v-col>
