2374f88a776284c6c23f42a2c408a8fb27b264a7
Valentin Youshkevich <valiantsin.yushkevich@playgendary.com>
2022-10-04 09:07:52 +0300

    separate components

---
+++ src/components/FiltersBlock.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-col cols="12" md="4">
+    <v-select v-model="variant" :items="items" clearable label="Filter by country"></v-select>
+
+    <v-select v-model="variant" :items="items" clearable label="Filter by score"></v-select>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props:{
+    items:{
+      required: true,
+      type: Array
+    }
+  },  
+  data() {
+    return {
+      variant: 'default'
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-col cols="12" md="4">
+    <v-card max-width="450" class="mx-auto">
+      <v-list three-line>
+        <template v-for="(item, index) in users">
+          <v-subheader v-if="item.header" :key="item.header" v-text="item.header"></v-subheader>
+
+          <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+
+          <v-list-item v-else :key="item.title">
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-card>
+
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    users: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
